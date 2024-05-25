f7dae76598cc9baf2ad8bd07bd63aeeb2f252287
Aleksey <vmstr@vmstr.ru>
2024-05-25 20:55:07 +0200

    Map component fully prepared to work with API

--- src/components/MapBlock/MapBlock.jsx
+++ src/components/MapBlock/MapBlock.jsx
@@ -6,14 +6,34 @@ import "./MapBlock.scss";
 const MapBlock = () => {
   const mapContainer = useRef(null);
   const map = useRef(null);
-
+  const myPoints = [
+    {
+      name: "some place name",
+      latitude: 44.762566353036185,
+      longitude: 20.42839050292969,
+    },
+    {
+      name: "some other",
+      latitude: 44.76963939259086,
+      longitude: 20.46684265136719,
+    },
+    {
+      name: "some another",
+      latitude: 44.75646821003823,
+      longitude: 20.453453063964847,
+    },
+  ];
   useEffect(() => {
     if (mapContainer.current && !map.current) {
       //set default position if no geolocation
-      map.current = L.map(mapContainer.current).setView(
-        [55.75017078646975, 37.60995090007783],
-        13
-      );
+      map.current = L.map(mapContainer.current, {
+        center: [55.75017078646975, 37.60995090007783],
+        zoom: 13,
+        zoomControl: false,
+      });
+      //reposition of zoom buttons to right corner
+      L.control.zoom({ position: "topright" }).addTo(map.current);
+      let currentPopup = null;
       //set map layer. Using OpenStreetMap
       L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
         attribution:
@@ -21,17 +41,30 @@ const MapBlock = () => {
       }).addTo(map.current);
       //adding functionality of clicks on map
       map.current.on("click", (e) => {
+        if (currentPopup) {
+          currentPopup.remove();
+        }
         const { lat, lng } = e.latlng;
-        console.log(`Clicked at: ${lat}, ${lng}`);
+        //Here is popup with coordinates and button to add it to DB
+        currentPopup = L.marker([lat, lng])
+          .addTo(map.current)
+          .bindPopup(
+            `This is a cool place. Coordinates:<br>latitude: ${lat},<br>longitude: ${lng}`
+          )
+          .openPopup();
       });
-
+      //Adding my points yo the map
+      myPoints.forEach((location) => {
+        const { name, latitude, longitude } = location;
+        L.marker([latitude, longitude]).addTo(map.current).bindPopup(name);
+      });
       //sending request to get user location
       if ("geolocation" in navigator) {
         navigator.geolocation.getCurrentPosition(
           (position) => {
             const lat = position.coords.latitude;
             const lng = position.coords.longitude;
-            map.current.setView([lat, lng], 13);
+            map.current.flyTo([lat, lng], 13);
           },
           (error) => {
             console.error("Error getting location:", error);
