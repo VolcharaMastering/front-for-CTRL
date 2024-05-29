92b452035f14d034a15c94694065f0cc95dcc917
Aleksey <vmstr@vmstr.ru>
2024-05-29 15:21:08 +0200

    configured to deploy to render server

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://back-for-ctrl.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
